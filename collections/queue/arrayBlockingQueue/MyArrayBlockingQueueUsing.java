dcf6c9e1ea65a412f5485ba387dbf062be29c657
webserg@gmail.com <webserg@gmail.com>
2014-05-20 21:50:18 +0300

    queue using lock and condition

--- collections/queue/arrayBlockingQueue/MyArrayBlockingQueueUsing.java
+++ collections/queue/arrayBlockingQueue/MyArrayBlockingQueueUsing.java
@@ -3,6 +3,9 @@ package collections.queue.arrayBlockingQueue;
 import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * Created by webserg on 14.05.2014.
@@ -10,7 +13,8 @@ import java.util.concurrent.Executors;
 public class MyArrayBlockingQueueUsing {
 
     public static void main(String[] args) {
-        MyArrayBlockingQueue q = new MyArrayBlockingQueue();
+//        MyArrayBlockingQueue q = new MyArrayBlockingQueue();
+        MyArrayBlockingQueueWithLock q = new MyArrayBlockingQueueWithLock();
         Runnable writers = () -> {
             Random randome = new Random();
             while (true) {
@@ -28,11 +32,13 @@ public class MyArrayBlockingQueueUsing {
         ExecutorService readerS = Executors.newFixedThreadPool(5);
         ExecutorService writerS = Executors.newFixedThreadPool(1);
         readerS.submit(readers);
-        readerS.submit(readers);
-        readerS.submit(readers);
-        readerS.submit(readers);
-        readerS.submit(readers);
+//        readerS.submit(readers);
+//        readerS.submit(readers);
+//        readerS.submit(readers);
+//        readerS.submit(readers);
         writerS.execute(writers);
+        readerS.shutdown();
+        writerS.shutdown();
 
     }
 
@@ -73,6 +79,54 @@ class MyArrayBlockingQueue {
             Integer t = queue[takeIdx++];
             queue.notifyAll();
             return t;
+        }
+    }
+}
+
+class MyArrayBlockingQueueWithLock {
+    int N = 10;
+    Integer[] queue = new Integer[N];
+    volatile int putIdx, takeIdx, countN = 0;
+    Lock lock = new ReentrantLock();
+    Condition cond = lock.newCondition();
+
+
+    void put(Integer t) {
+        lock.lock();
+        try {
+            while (countN == N) {
+                try {
+                    cond.await();
+                } catch (InterruptedException e) {
+                }
+            }
+            if (putIdx == N - 1) putIdx = 0;
+            countN++;
+            queue[putIdx++] = t;
+            cond.signalAll();
+        } finally {
+           lock.unlock();
+        }
+
+    }
+
+    Integer get() {
+       lock.lock();
+        try {
+            while (countN == 0) {
+                try {
+                    cond.await();
+                } catch (InterruptedException e) {
+                }
+            }
+            if (takeIdx == N - 1) takeIdx = 0;
+            countN--;
+
+            Integer t = queue[takeIdx++];
+            cond.signalAll();
+            return t;
+        } finally {
+           lock.unlock();
         }
     }
 }
